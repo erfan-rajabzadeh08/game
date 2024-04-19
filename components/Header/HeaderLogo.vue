042f9177ce49d461d196dec5690d3a6dbed17e24
erfan rajabzadeh <erfan1380rz@gmail.com>
2024-04-19 16:08:57 +0330

    Revert "game project"
    This reverts commit 11ec2986ac632be973461308eb47b2a1ffa6eb5b.

--- components/Header/HeaderLogo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <!-- Header Logo Start -->
-    <NuxtLink to="/">
-        <img class="logo" src="/images/logo.webp" alt="Header Logo">
-    </NuxtLink>
-    <!-- Header Logo End -->
-</template>
-
-
